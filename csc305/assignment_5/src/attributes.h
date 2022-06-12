bc57064e505ff39475dfaa41a672b02b14296c43
brianalynette <79673623+brianalynette@users.noreply.github.com>
2022-06-12 11:31:46 -0700

    Add files via upload

---
+++ csc305/assignment_5/src/attributes.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <Eigen/Core>
+
+class VertexAttributes
+{
+public:
+    VertexAttributes(float x = 0, float y = 0, float z = 0, float w = 1)
+    {
+        position << x, y, z, w;
+        color << 1, 1, 1;
+        normal << 1, 1, 1, 1;
+    }
+
+    // Interpolates the vertex attributes
+    static VertexAttributes interpolate(
+        const VertexAttributes &a,
+        const VertexAttributes &b,
+        const VertexAttributes &c,
+        const float alpha,
+        const float beta,
+        const float gamma)
+    {
+        VertexAttributes r;
+        r.position = alpha * a.position + beta * b.position + gamma * c.position;
+        r.color = alpha * a.color + beta * b.color + gamma * c.color;
+        r.normal = (c.normal - a.normal).cwiseProduct(b.normal - a.normal).normalized();
+        return r;
+    }
+
+    Eigen::Vector4f position;
+    Eigen::Vector3f color;
+    Eigen::Vector4f normal;
+};
+
+class FragmentAttributes
+{
+public:
+    FragmentAttributes(float r = 0, float g = 0, float b = 0, float a = 1)
+    {
+        color << r, g, b, a;
+    }
+    Eigen::Matrix4f camera;
+    Eigen::Vector4f color;
+    Eigen::Vector4f position;
+};
+
+class FrameBufferAttributes
+{
+public:
+    FrameBufferAttributes(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, uint8_t a = 255)
+    {
+        color << r, g, b, a;
+        depth = 150; // far plane
+    }
+
+    Eigen::Matrix<uint8_t, 4, 1> color;
+    float depth;
+};
+
+class UniformAttributes
+{
+public:
+    Eigen::Matrix4f projection;
+    Eigen::Matrix4f camera;
+    Eigen::Vector4f color;
+    Eigen::Matrix4f transformation;
+};
